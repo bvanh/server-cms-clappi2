1fbb49d089bc914d4d5eb50ea3b7a3e461f31aa5
vietahn <bv.anh194@gmail.com>
2020-10-22 00:41:20 +0700

    add connect db

--- api/controls/UserController.js
+++ api/controls/UserController.js
@@ -1,5 +1,6 @@
 "use strict";
 const jwtService = require("../services/auth.services");
+const db = require("../services/db.services");
 let users = {
   anhviet: "123123",
 };
@@ -18,6 +19,13 @@ module.exports = {
   },
   getUsers: (req, res, next) => {
     try {
+      let sql = "UPDATE fake_info SET currentMail = currentMail + 500";
+      db.query(sql, (err, response) => {
+        if (err) {
+          res.status(400).send(err);
+        }
+        console.log("add fake success");
+      });
       return res.status(200).json({ users });
     } catch (err) {
       return res.status(500).json({ msg: "Internal server error" });
